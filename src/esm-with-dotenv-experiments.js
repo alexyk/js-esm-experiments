a6d606916bcd567353adb4e5bd3235c13868924b
alexyk <370500+alexyk@users.noreply.github.com>
2019-09-30 20:08:53 +0300

    Better logging

--- src/esm-with-dotenv-experiments.js
+++ src/esm-with-dotenv-experiments.js
@@ -4,9 +4,15 @@ import RunExperiments from './definitions/RunExperiments'
 class EsmExperiments extends RunExperiments {
   runLogWithObjectIssue() {
     const origLog = console.log;
-    const args = ['hello there'];
-    const log = function(...a) { origLog(...a) };
+
+    const args = ['hello there - with issue'];
+    let log = function(...a) { origLog(...a) };
+
+    console.log('%cObject issue:', 'color:green');
     log(...args);
+
+    console.log('%cNormal print:','color:green');
+    console.log('hello there - normal:', 'with', 'several', 'params');
   }
 }
 
